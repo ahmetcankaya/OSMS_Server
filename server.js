c8e92ec11653f104316c9925dc8aedc1175fd8cb
ahmetcankaya <ahmetcankaya11@gmail.com>
2020-09-05 00:21:39 +0300

    Create

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path")
+
+const app = express();
+app.use(express.static('OSMS-UI'))
+
+app.get("/ok", (req,res,next) =>{
+    res.status(200).send('ok')
+})
+
+app.use((req,res,next) =>{
+   return res.sendFile(path.join(__dirname+'/OSMS-UI/index.html'));
+})
+
+app.listen(process.env.PORT||3000, ()=>{
+    console.log(`AP is started on port : ${process.env.PORT||3000}`)
+})

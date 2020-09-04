b47d688bc0e11db20a4775e0116c0a5761d351f7
ahmetcankaya <ahmetcankaya11@gmail.com>
2020-09-05 00:34:31 +0300

    Createa Again

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const path = require("path")
 
 const app = express();
-app.use(express.static('OSMS-UI'))
+app.use(express.static('public'))
 
 app.get("/ok", (req,res,next) =>{
     res.status(200).send('ok')
 })
 
 app.use((req,res,next) =>{
-   return res.sendFile(path.join(__dirname+'/OSMS-UI/index.html'));
+   return res.sendFile(path.join(__dirname+'/public/index.html'));
 })
 
 app.listen(process.env.PORT||3000, ()=>{
